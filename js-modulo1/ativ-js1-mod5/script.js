dd8548c08667965cf1e44f290fbc71f4f278d73c
Quezia Guilhermina Dias dos Santos <queziagds@gmail.com>
2023-10-14 21:59:58 -0300

    enviando atividade

---
+++ js-modulo1/ativ-js1-mod5/script.js
@@ -0,0 +1,5 @@
+let titulo = document.getElementById('titulo');
+titulo.innerText = 'Aprendendo a manipular DOM';
+
+let ul = document.querySelector('ul');
+ul.innerHTML = "<ul><li> Elemento de lista JS 01</li><li> Elemento de lista JS 01</li><li> Elemento de lista JS 01</li></ul>";
